f59e7ebd21d37760038054413b9076a5b2779bc0
severusamit <87641622+severusamit@users.noreply.github.com>
2024-01-18 23:21:49 +0530

    NestedLoopChallenge

---
+++ Hello/NestedLoopChallenge.py
@@ -0,0 +1,14 @@
+numbers = [5,2,5,2,2]
+for i in numbers:
+    output = ''
+    for count in range(i):
+        output +='x'
+    print(output)
+    #print(i * "x")
+print("---------------------")
+num =[1,1,1,1,5]
+for j in num:
+    out=''
+    for count_L in range(j):
+        out += 'x'
+    print(out)
