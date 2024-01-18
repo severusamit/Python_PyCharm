d0bfadf8959f5701b44695c4a1f29643b7ce16dc
severusamit <87641622+severusamit@users.noreply.github.com>
2024-01-18 13:26:56 +0530

    String Method

---
+++ Hello/StringMethods.py
@@ -0,0 +1,8 @@
+course = 'Python for beginners'
+print(len(course))
+print(course.upper())
+print(course.lower())
+print(course.find('b'))
+print(course.replace('for','for new'))
+print('Python' in course)
+print(course.title())
