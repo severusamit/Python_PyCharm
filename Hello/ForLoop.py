0cb4df97049bb73f55c4a00f1d792eb492eeebad
severusamit <87641622+severusamit@users.noreply.github.com>
2024-01-18 14:14:40 +0530

    For Loop

---
+++ Hello/ForLoop.py
@@ -0,0 +1,11 @@
+for item in range(10,0,-1):
+    print(item)
+
+for item in ['JOSH','AMIT']:
+    print(item)
+
+prices = [10,20,30]
+total=0
+for item in prices :
+    total +=item
+print(f"total : {total}")
