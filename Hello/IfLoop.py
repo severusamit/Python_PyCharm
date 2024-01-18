f66bc25b64086b013d28d5b6cead8d5ee26d1a75
severusamit <87641622+severusamit@users.noreply.github.com>
2024-01-18 13:28:45 +0530

    If Loop

---
+++ Hello/IfLoop.py
@@ -0,0 +1,11 @@
+is_hot= False
+is_cold=True
+if is_hot :
+    print("it's a hot day")
+    print("Drinking plenty of water")
+elif is_cold:
+    print("It's a cold day")
+    print("Wear warm clothes")
+else :
+    print("It's a lovely day")
+print("Enjoy your day")
