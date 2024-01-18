16b2b4b5bc907b6ed9a61af81d56b7a01c36548f
severusamit <87641622+severusamit@users.noreply.github.com>
2024-01-18 13:27:23 +0530

    ComparisonOperator

---
+++ Hello/ComparisonOperator.py
@@ -0,0 +1,17 @@
+temp =30
+if temp < 30 :
+    print("It's hot day")
+else :
+    print("It's not hot day")
+
+print("Exercise")
+print("-----------------------------")
+
+name = input("Enter name  ? ")
+if   len(name) < 3 :
+    print("Name must be atleast 3 character")
+elif len(name) >50:
+    print("Name must be a max of 50 char")
+else :
+    print("Name looks good")
+
