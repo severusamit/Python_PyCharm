7abbc0dc0e6c941b0452a64518080404d96c1c05
severusamit <87641622+severusamit@users.noreply.github.com>
2024-01-18 13:26:47 +0530

    String

---
+++ Hello/Strings.py
@@ -0,0 +1,12 @@
+course = "Python's Course For Beginners"
+print(course[0])
+print(course[-2])
+print(course[1:])
+print(course[:5])
+another=course[:]
+print(another)
+print(course[:])  #copy the string
+
+print('Exercise')
+name = 'Jennifer'
+print(name[1:-1])
