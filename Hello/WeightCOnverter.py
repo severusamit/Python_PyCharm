88ac65f83c4528a7154e0c98f78f91e007ba035c
severusamit <87641622+severusamit@users.noreply.github.com>
2024-01-18 14:14:27 +0530

    Weight COnverter

--- Hello/WeightCOnverter.py
+++ Hello/WeightCOnverter.py
@@ -1,5 +1,9 @@
 weight =int(input("Weight :"))
 unit =input("(L)bs or (K)g : ")
 if unit =='L' or unit =='l' :
+    # or unit.upper() =="L"
     converted = weight * .45
     print(f"you are {converted} kilos")
+else :
+    converted = weight /.45
+    print("You are "+str(converted) + "pounds")
