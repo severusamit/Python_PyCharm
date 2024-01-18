2acd0ad70b4ae074e3e99a22ab4dba2e8e969a98
severusamit <87641622+severusamit@users.noreply.github.com>
2024-01-18 14:14:35 +0530

    Gueesing Game

---
+++ Hello/GuessingGame.py
@@ -0,0 +1,11 @@
+secretNumber =9
+guess_count=0
+guess_limit=3
+while guess_count<guess_limit :
+    guess =(int(input("Guess :")))
+    guess_count=guess_count+1
+    if(guess==secretNumber):
+        print("You win!")
+        break
+else :
+    print("Sorry, you failed")
