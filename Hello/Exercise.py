c21263310921f684b516d29dc5855f7ef965e920
severusamit <87641622+severusamit@users.noreply.github.com>
2024-01-18 13:28:37 +0530

    Exercise

---
+++ Hello/Exercise.py
@@ -0,0 +1,11 @@
+# Exercise 1
+age = 20
+name = "John Smith"
+is_new= True
+print(name,age,is_new)
+
+# Exercise 2
+name = input("Enter persons name ? ")
+color=input("Enter fav color ? ")
+print(name + " likes " +color)
+
